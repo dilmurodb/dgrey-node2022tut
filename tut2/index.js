4d0105afcde4ee837023b271aff8a3edfa99301a
Dilmurod Bukharov <dilmurod.bukharov@gmail.com>
2025-11-21 14:56:58 -0500

    feat: work with file systems module existsSync methods

--- tut2/index.js
+++ tut2/index.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const fsPromises = require('fs').promises;
 const path = require('path');
 
 
@@ -7,27 +7,39 @@ const path = require('path');
 //     console.log(data);
 // })
 
-fs.readFile(path.join(__dirname, 'files', 'starter.txt'), 'utf8', (err, data) => {
-    if (err) throw err;
-    console.log(data);
-})
+const fileOps = async () => {
+    try {
+       const data = await fsPromises.readFile(path.join(__dirname, 'files', 'starter.txt'), 'utf8');
+       console.log(data);
+       await fsPromises.unlink(path.join(__dirname, 'files', 'starter.txt'))
+       await fsPromises.writeFile(path.join(__dirname, 'files', 'writeFile.txt'), data);
+       await fsPromises.appendFile(path.join(__dirname, 'files', 'writeFile.txt'), '\n\nHello nice to meet you, my name is Smith');
+       await fsPromises.rename(path.join(__dirname, 'files', 'writeFile.txt'), path.join(__dirname, 'files', 'newWriteFile.txt'));
+       const newData = await fsPromises.readFile(path.join(__dirname, 'files', 'newWriteFile.txt'), 'utf8')
+       console.log(newData);
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+fileOps();
 
 
 
-fs.writeFile(path.join(__dirname, 'files', 'reply.txt'), 'Nice to meet you!', (err) => {
-    if (err) throw err;
-    console.log('Write complete!')
+// fs.writeFile(path.join(__dirname, 'files', 'reply.txt'), 'Nice to meet you!', (err) => {
+//     if (err) throw err;
+//     console.log('Write complete!')
 
-    fs.appendFile(path.join(__dirname, 'files', 'reply.txt'), '\n\nLikewise', (err) => {
-    if (err) throw err;
-    console.log('Append complete!')
+//     fs.appendFile(path.join(__dirname, 'files', 'reply.txt'), '\n\nLikewise', (err) => {
+//     if (err) throw err;
+//     console.log('Append complete!')
 
-    fs.rename(path.join(__dirname, 'files', 'reply.txt'), path.join(__dirname, 'files', 'newReply.txt'), (err) => {
-        if (err) throw err;
-        console.log('Rename complete')
-    })
-    })
-})
+//     fs.rename(path.join(__dirname, 'files', 'reply.txt'), path.join(__dirname, 'files', 'newReply.txt'), (err) => {
+//         if (err) throw err;
+//         console.log('Rename complete')
+//     })
+//     })
+// })
 
 
 
